e80a9dc7edb79def32f50733d9535bb49d1f8691
Nathan Rougier <nathan.rougier@gmail.com>
2021-10-27 15:46:53 +0200

    adaptation of dropnan

--- pre_pro_treat.py
+++ pre_pro_treat.py
@@ -1,6 +1,7 @@
 import numpy as np
+from numpy.core.numeric import NaN
 
-def dropnan(X, y, missing_feature):
+def dropnan(X=None, y=None, missing_feature=NaN):
     '''Drops the instances that have a missing value for the given feature.
     
     The input dataset (X, y) is not modified. A new dataset is created and returned.
@@ -20,8 +21,16 @@ def dropnan(X, y, missing_feature):
             A tuple (X_nomissing, y_nomissing) that corresponds to the input dataset (X, y) without the instances
             with missing values.
     '''
-    X_nomissing = X.copy()
-    y_nomissing = y.copy()
-    y_nomissing = np.delete(y_nomissing, np.argwhere(np.isnan(X_nomissing[:, missing_feature])), axis=0)
-    X_nomissing = X_nomissing[~np.isnan(X_nomissing).any(axis=1)]
-    return (X_nomissing, y_nomissing)
+    res = list()
+    if not(X is None):
+        X_nomissing = X.copy()
+        X_nomissing = X_nomissing[~np.isnan(X_nomissing).any(axis=1)]
+        res.append(X_nomissing)
+    if not(y is None):
+        y_nomissing = y.copy()
+        y_nomissing = np.delete(y_nomissing, np.argwhere(np.isnan(X_nomissing[:, missing_feature])), axis=0)
+        res.append(y_nomissing)
+    if (len(res) > 1):
+        return tuple(res)
+    else:
+        return res[0]
